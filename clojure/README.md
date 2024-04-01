f488d06124db49d9f1b0ecec9e6641ac7f4ee7b8
Denis Danilov <ddanilov@users.noreply.github.com>
2024-04-01 21:24:45 +0200

    clojure: command line interface

--- clojure/README.md
+++ clojure/README.md
@@ -12,3 +12,9 @@ Unit tests can be executed with command
     clojure -X:test/kaocha
 
 It will also generate code coverage report in `target/coverage` directory.
+
+## Run
+
+To read input data from a file and print the distribution results run command
+
+    clojure -M:run/main <path-to-file>
--- clojure/src/lrm/hare.clj
+++ clojure/src/lrm/hare.clj
@@ -27,3 +27,19 @@
   (loop [i 0, xs (vec (sort-by #(nth % 2) > quotas))]
     (if (= i res) xs
         (recur (inc i), (update-in xs [i 1] inc)))))
+
+(defn print-results [data]
+  (let [print-line (fn [v1 v2 v3] (println (format "%-10s %10s %10s" (str v1) (str v2) (str v3))))]
+    (print-line "name" "result" "remainder")
+    (doseq [[v1 v2 v3] (sort-by second > data)]
+      (print-line v1 v2 v3))))
+
+(defn -main [filename]
+  (let [d (read-data filename)
+        q (distribute-quota d)
+        f (distribute-rest q)]
+    (println "============ QUOTA =============")
+    (print-results (second q))
+    (println (format "rest: %d" (first q)))
+    (println "============ FINAL =============")
+    (print-results f)))
